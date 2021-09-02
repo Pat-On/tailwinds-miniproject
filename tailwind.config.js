b8118581009ab68b513bd82c81682efc1d282ff1
Patryk <patryk.r.nowak@gmail.com>
2021-09-02 17:11:45 +0100

    formating the fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     debugScreens: {
       position: ['top', 'left']
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        // by this we defined our own font -> nice
+        headline: ["Oswald"]
+      },
+      colors: {
+        // mainColor is our name, we can put it whatever we want
+        // so we extended again the tailwin! nice!
+        mainColor: '#212f49'
+      }
+    },
   },
   variants: {
     extend: {},
